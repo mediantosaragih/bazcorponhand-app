ba7b21b465c632cc01ada4930eed3a4a2b0c757f
Rio Efraim Simanjuntak <70948510+rioefraim@users.noreply.github.com>
2024-02-17 21:03:37 +0700

    Update main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -337,6 +337,23 @@
                             </a>
                         </li>
                         <li class="nav-item">
+                            <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-wrench"></i>
+                            <p>
+                                Setting
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{route('admin.setting.time-and-attendance')}}" class="nav-link">
+                                <i class="far fa-clock nav-icon"></i>
+                                <p>Time & Attendance</p>
+                                </a>
+                            </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
                             <a href="{{route('logout')}}" class="nav-link">
                                 <i class="nav-icon fas fa-th"></i>
                                 <p>
